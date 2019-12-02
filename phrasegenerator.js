bd9d046f178df4e489c73603cd8f91129bee3301
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-02 14:50:27 -0600

    Added script to load blog entries by their IDs.

--- phrasegenerator.js
+++
@@ -1,9 +0,0 @@
-const phrases = [
-	'Hello there!',
-	'Eat my shorts',
-	'UPPER TEXT'
-]
-
-function generate_phrases(phrases){
-	return phrases[Math.floor(Math.random()*phrases.length)];
-}
--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-	$('header').load('header.html');
-	$('footer').load('footer.html');
+	$(`header`).load(`header.html`);
+	$(`footer`).load(`footer.html`);
+	// Load a blog entry as determined by its ID.
+	let blogIndex = $(`article`).attr(`id`);
+	$(`article`).load(`blog/blog_${blogIndex}.html`);
 });
