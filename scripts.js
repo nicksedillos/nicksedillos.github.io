51b489d63b9c1ce7507c87b0494ab066790b3929
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-04 12:16:31 -0600

    Gallery modal WIP.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Load templates when called in the document.
 $(document).ready(function(){
 	$(`template#head`).load(`/templates/head.html`);
 	$(`header`).load(`/templates/header.html`);
@@ -6,3 +7,8 @@ $(document).ready(function(){
 	let articleIndex = $(`article`).attr(`id`);
 	$(`article`).load(`/articles/${articleIndex}.html`);
 });
+
+// Load gallery_image modal when thumbnails are clicked.
+$(`.thumbnail`).onclick = function(){
+	$(`div.gallery_image`).load(`templates/modals/gallery_image.html`);
+}
