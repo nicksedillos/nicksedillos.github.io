0fe309cd4358ca9a27fbb803fe30426eb938e0dd
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-09 15:22:37 -0600

    Got thumbnails and links to fullsize images working in sketchbook.

--- templates/image_gallery.js
+++ templates/image_gallery.js
@@ -1,7 +1,8 @@
+// Use the gallery_contents object to assemble a set of thumbnails, each of which opens a lightbox modal when clicked. Not finished.
 $(document).ready(function(){
-	$.each(gallery_contents, function(i, val) {
-		$(`div.image_gallery`).html(`
-			<img src=$(filename)>
+	$.each(gallery_contents, function(index, value){
+		$(`div.image_gallery`).append(`
+			<a href="${this.fullsize}"><img class="thumbnail" src="${this.smallsize}" alt="${this.caption}"></a>
 		`);
-	}
+	});
 });
