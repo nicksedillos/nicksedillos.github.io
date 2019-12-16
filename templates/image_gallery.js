1f2c5600905afc649d551c8977a01608903ae1f1
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-16 16:03:07 -0600

    WIP.

--- templates/image_gallery.js
+++ templates/image_gallery.js
@@ -1,13 +1,3 @@
-// See: https://www.w3schools.com/howto/howto_js_lightbox.asp
-// const lightbox = `
-// 	<div id="lightbox">
-// 		<span class="close_button">×</span><img src=${gallery_contents[1].fullsize}>
-// 		<div class="lightbox_caption">
-// 			<p>${gallery_contents[1].caption}</p>
-// 		</div>
-// 	</div>
-// `;
-
 const open_lightbox = function (){
 	document.getElementById("lightbox").style.display = "block";
 };
@@ -18,6 +8,7 @@ const close_lightbox = function(){
 
 // Use the gallery_contents object to assemble a set of thumbnails, each of which opens a lightbox modal when clicked. Not finished.
 $(document).ready(function(){
+	$(`div#lightbox`).load(`/templates/modals/lightbox.html`);
 	$.each(gallery_contents, function(index, value){
 		$(`div#image_gallery`).append(`
 			<img class="thumbnail" src="${this.smallsize}" alt="${this.caption}">
