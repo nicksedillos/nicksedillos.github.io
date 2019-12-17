323e023c6a11293170feb13a9d6645471cff5651
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-17 15:53:00 -0600

    Image gallery now opens and closes the correct images.

--- templates/image_gallery.js
+++ templates/image_gallery.js
@@ -1,18 +1,23 @@
-const open_lightbox = function (){
-	document.getElementById("lightbox").style.display = "block";
-};
-
 const close_lightbox = function(){
 	document.getElementById("lightbox").style.display = "none";
-}
+};
 
-// Use the gallery_contents object to assemble a set of thumbnails, each of which opens a lightbox modal when clicked. Not finished.
+const open_lightbox = function(index){
+	document.getElementById("lightbox").style.display = "block";
+	$(`.close_button`).click(close_lightbox);
+	$(`#lightbox_image_box`).html(`
+		<img src="${gallery_contents[index].fullsize}">
+	`);
+	$(`#lightbox_caption_box`).html(`
+		<p>${gallery_contents[index].caption}</p>
+	`)
+};
+
 $(document).ready(function(){
 	$(`div#lightbox`).load(`/templates/modals/lightbox.html`);
 	$.each(gallery_contents, function(index, value){
 		$(`div#image_gallery`).append(`
-			<img class="thumbnail" src="${this.smallsize}" alt="${this.caption}">
+			<img class="thumbnail" onclick="open_lightbox(${index})" src="${this.smallsize}" alt="${this.caption}">
 		`);
-		$(`.close_button`).click(close_lightbox);
 	});
 });
