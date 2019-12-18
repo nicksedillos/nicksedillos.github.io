75136a93bd4ae93a4690550b24cb86c206def87e
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-18 15:33:27 -0600

    Lightbox arrow buttons now work.

--- templates/image_gallery.js
+++ templates/image_gallery.js
@@ -1,23 +1,42 @@
+// Hide the lightbox.
 const close_lightbox = function(){
 	document.getElementById("lightbox").style.display = "none";
 };
 
-const open_lightbox = function(index){
+// Decrease the current_index_number by 1, then re-render lightbox.
+const lightbox_previous = function(){
+	current_lightbox_index = current_lightbox_index - 1;
+	display_lightbox();
+};
+
+// Increase the current_index_number by 1, then re-render lightbox.
+const lightbox_next = function(){
+	current_lightbox_index = current_lightbox_index + 1;
+	display_lightbox();
+};
+
+// Display the lightbox at the current_lightbox_index number.
+const display_lightbox = function(){
 	document.getElementById("lightbox").style.display = "block";
-	$(`.close_button`).click(close_lightbox);
 	$(`#lightbox_image_box`).html(`
-		<img src="${gallery_contents[index].fullsize}">
+		<img src="${gallery_contents[current_lightbox_index].fullsize}">
 	`);
 	$(`#lightbox_caption_box`).html(`
-		<p>${gallery_contents[index].caption}</p>
+		<p>${gallery_contents[current_lightbox_index].caption}</p>
 	`)
 };
 
+// Display the image gallery thumbnails with contents as defined by the gallery_contents array.
 $(document).ready(function(){
-	$(`div#lightbox`).load(`/templates/modals/lightbox.html`);
+	$(`div#lightbox`).load(`/templates/lightbox.html`);
 	$.each(gallery_contents, function(index, value){
 		$(`div#image_gallery`).append(`
-			<img class="thumbnail" onclick="open_lightbox(${index})" src="${this.smallsize}" alt="${this.caption}">
+			<img class="thumbnail" data-index="${index}"; src="${this.smallsize}" alt="${this.caption}">
 		`);
+	});
+	$(`.thumbnail`).click(function(){
+		current_lightbox_index = Number($(this).attr(`data-index`));
+		display_lightbox(current_lightbox_index);
+		console.log(current_lightbox_index);
 	});
 });
