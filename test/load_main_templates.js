b1029d7928f25f6f22f3a1bb7bad2e32d86c0f83
Nick Sedillos <nick.sedillos@gmail.com>
2019-12-23 12:52:39 -0600

    Deleted test folder.

--- test/load_main_templates.js
+++
@@ -1,9 +0,0 @@
-// Load templates when called in the document.
-$(document).ready(function(){
-	$(`template#head`).load(`/templates/head.html`);
-	$(`header`).load(`/templates/header.html`);
-	$(`footer`).load(`/templates/footer.html`);
-	// Load each article by its ID.
-	let articleIndex = $(`article`).attr(`id`);
-	$(`article`).load(`/articles/${articleIndex}.html`);
-});
