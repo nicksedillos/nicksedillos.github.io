7fc85241ec30237e75909aa2661a79153c021615
Nick Sedillos <nick.sedillos@gmail.com>
2020-01-04 16:41:21 -0600

    Moved head.htm template back into individual documents.

--- load_main_templates.js
+++ load_main_templates.js
@@ -1,6 +1,5 @@
 // Load templates when called in the document.
 $(document).ready(function(){
-	$(`template#head`).load(`/templates/head.html`);
 	$(`header`).load(`/templates/header.html`);
 	$(`footer`).load(`/templates/footer.html`);
 	// Load each article by its ID.
