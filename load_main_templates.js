5d5c2d65ae0914e564647e41770823668e54b58f
Nick Sedillos <nick.sedillos@gmail.com>
2020-01-06 10:35:45 -0600

    Added function to automatically insert boilerplate before and after articles.

--- load_main_templates.js
+++ load_main_templates.js
@@ -3,8 +3,18 @@ $(document).ready(function(){
 	$(`template#head`).load(`/templates/head.html`);
 	$(`header`).load(`/templates/header.html`);
 	$(`footer`).load(`/templates/footer.html`);
-	// Load each article by its ID.
+	// Load each article by its ID, then prepend an anchor link and append a link to the top of the page and a divider.
 	document.querySelectorAll(`article`).forEach(function(element){
-		$(element).load(`/articles/${element.id}.html`);
+		$.ajax(`/articles/${element.id}.html`).done(function(content){
+			$(element).html(content);
+		}).done(function(){
+			$(element).prepend(`
+				<a id="#${element.id}"></a>
+			`);
+			$(element).append(`
+				<p><a href="#top">^</a></p>
+				<p class="centered">⁂</p>
+			`);
+		});
 	});
 });
