9f8e9d526e3245c48aab055c7e82f35ec271ab3a
Nick Sedillos <nick.sedillos@gmail.com>
2020-01-14 15:31:12 -0600

    WIP. Trying to revise header.html.

--- load_main_templates.js
+++ load_main_templates.js
@@ -1,9 +1,9 @@
-const load_glyphs = function(){
-	$(`span.glyph`).each(function(){
-		let glyph_index = $(this).attr(`data`);
-		$(this).load(`/glyphs/${glyph_index}.svg`);
-	});
-};
+// const load_glyphs = function(){
+// 	$(`span.glyph`).each(function(){
+// 		let glyph_index = $(this).attr(`data`);
+// 		$(this).load(`/glyphs/${glyph_index}.svg`);
+// 	});
+// };
 
 const load_articles = function(){
 	document.querySelectorAll(`article`).forEach(function(element){
@@ -19,7 +19,7 @@ const load_articles = function(){
 				<p><a class="link_to_top" href=".">^</a></p>
 				<span class="glyph inline-1rem" data="divider">
 			`);
-			load_glyphs();
+			// load_glyphs();
 		});
 	});
 };
